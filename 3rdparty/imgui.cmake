45d67502ddb87e86b0adb9ce412149fb30bdfb5a
lanxing002 <scent_wen@outlook.com>
2024-10-29 14:57:31 +0800

    add imgui

---
+++ 3rdparty/imgui.cmake
@@ -0,0 +1,32 @@
+message(--<start==config==imgui>)
+set(imgui_SOURCE_DIR_ ${CMAKE_CURRENT_SOURCE_DIR}/imgui)
+
+## imgui sorce files
+file(GLOB imgui_sources CONFIGURE_DEPENDS  "${imgui_SOURCE_DIR_}/*.cpp")
+
+## imgui dependence graphics-api files
+file(GLOB imgui_impl CONFIGURE_DEPENDS  
+"${imgui_SOURCE_DIR_}/backends/imgui_impl_glfw.cpp" 
+"${imgui_SOURCE_DIR_}/backends/imgui_impl_glfw.h"
+"${imgui_SOURCE_DIR_}/backends/imgui_impl_opengl3.cpp" 
+"${imgui_SOURCE_DIR_}/backends/imgui_impl_opengl3.h" 
+"${imgui_SOURCE_DIR_}/backends/imgui_impl_opengl3_loader.h")
+add_library(imgui STATIC ${imgui_sources} ${imgui_impl})
+target_include_directories(imgui PUBLIC $<BUILD_INTERFACE:${imgui_SOURCE_DIR_}>)
+target_include_directories(imgui PUBLIC $<BUILD_INTERFACE:${imgui_SOURCE_DIR_}/backends>)
+target_link_libraries(imgui PUBLIC glfw)
+
+option(IMGUI_SIMPLE_EXAMPLE "Build the imgui simple example program" ON)
+if (IMGUI_SIMPLE_EXAMPLE)
+    add_executable(imgui-test WIN32 ${CMAKE_CURRENT_SOURCE_DIR}/imgui/examples/example_glfw_opengl3/main.cpp)
+    target_link_libraries(imgui-test PUBLIC imgui ${gl3_lib})
+    
+    if (MSVC)
+        # Tell MSVC to use main instead of WinMain
+        set_target_properties(imgui-test PROPERTIES LINK_FLAGS "/ENTRY:mainCRTStartup")
+    endif()
+endif()
+
+message(--<end==config==imgui>)
+
+
