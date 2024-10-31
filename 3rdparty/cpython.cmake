3bf7e5071d2d3c187b13a861f3e0b8a077749ae9
lanxing002 <lanxing002@lanxing.com>
2024-10-31 10:42:49 +0800

    batter config

--- 3rdparty/cpython.cmake
+++ 3rdparty/cpython.cmake
@@ -1,21 +1,10 @@
-message("-- <start config cpython>--")
-message("")
-message("")
+message("-- ===== <start config cpython> ====")
+
 set(cpython_dir_ ${CMAKE_CURRENT_SOURCE_DIR}/cpython)
 
-## wrong solution from chagpt
-# include(ExternalProject)
-# ExternalProject_Add(
-#     cpython
-#     SOURCE_DIR "${cpython_dir_}/Python"
-#     CONFIGURE_COMMAND ""
-#     BUILD_COMMAND "msbuild ${cpython_dir_}/PCbuild/pythoncore.vcxproj"
-#     INSTALL_COMMAND ""
-# )
-
-
-# include_external_msproject(cpythoncore "${cpython_dir_}/PCbuild/pythoncore.vcxproj")
-# set_target_properties(cpythoncore PROPERTIES FOLDER ${third_party_folder}/cpython)
+# 引入vs项目
+include_external_msproject(cpythoncore "${cpython_dir_}/PCbuild/pythoncore.vcxproj")
+set_target_properties(cpythoncore PROPERTIES FOLDER ${third_party_folder}/cpython)
 
 execute_process(
     COMMAND ${CMAKE_COMMAND} -E echo "staring runging build python......"
@@ -29,19 +18,25 @@ if(NOT result EQUAL 0)
     message(FATAL_ERROR "build python failed with exit code ${result}")
 endif()
 
-add_library(pylib INTERFACE)
-target_include_directories(pylib INTERFACE "$<BUILD_INTERFACE:${cpython_dir_}/build/Include>")
+add_library(pylib SHARED IMPORTED)
+set_property(TARGET pylib PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${cpython_dir_}/build/Include)
+set(pybindir ${cpython_dir_}/PCbuild/amd64)
+set_target_properties(pylib
+	PROPERTIES
+	IMPORTED_IMPLIB_DEBUG  ${cpython_dir_}/PCbuild/amd64/python310_d.lib
+	IMPORTED_IMPLIB_RELEASE  ${cpython_dir_}/PCbuild/amd64/python310.lib
+ 	)
 
-find_library(pylib_obj NAMES python310 PATHS ${cpython_dir_}/PCbuild/amd64)
-target_link_libraries(pylib INTERFACE ${pylib_obj})
+# add_library(pylib INTERFACE)
+# find_library(pylib_compiled_d NAMES python310_d PATHS ${cpython_dir_}/PCbuild/amd64)
+# find_library(pylib_compiled_r NAMES python310 PATHS ${cpython_dir_}/PCbuild/amd64)
 
-# add_library(py_empty STATIC ${cpython_dir_}/PCbuild/empty.cpp)
-# add_custom_command(
-#     TARGET py_empty POST_BUILD
-#     COMMAND ${cpython_dir_}/PCbuild/build.bat
-#     COMMAND ${cpython_dir_}/PCbuild/deploy.bat
-# )
-# set_target_properties(py_empty PROPERTIES FOLDER ${third_party_folder}/cpython)
-message("")
-message("")
-message("-- <end config cpython>--")
+# message(STATUS "found debug library ${pylib_obj_d}")
+# message(STATUS "found release library ${pylib_obj_r}")
+
+# target_include_directories(pylib INTERFACE "$<BUILD_INTERFACE:${cpython_dir_}/build/Include>")
+# target_link_libraries ( pylib INTERFACE
+# 						debug ${pylib_compiled_d}
+# 						optimized ${pylib_compiled_r} )
+
+message("-- ===== <end config cpython> ====")
