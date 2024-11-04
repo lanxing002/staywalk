b2aca8b38bd07f6f625c90c63541feb062d48728
lanxing002 <lanxing002@lanxing.com>
2024-11-04 19:58:34 +0800

    dump stash

--- src/engine/Utility.h
+++ src/engine/Utility.h
@@ -5,6 +5,8 @@
 namespace staywalk{
 	class Utility{
 	public:
+		static const string kFileExt;
+
 		static int64_t GetRandomId();
 
 		static bool check_ifstream(const std::ifstream& fstrm);
@@ -12,6 +14,26 @@ namespace staywalk{
 		static bool check_ofstream(const std::ofstream& fstrm);
 
 		static void load_model(const string & path);
+	};
+
+	class Dumper {
+	public:
+		enum class Status {
+			Wait,
+			Dumping,
+			Done
+		};
+		
+		Dumper() {}
+		Dumper(const Dumper&) = delete;
+		Dumper(Dumper&&) = delete;
+		Dumper& operator=(const Dumper&) = delete;
+		Dumper& operator=(Dumper&&) = delete;
+
+		void dump_in_file(shared_ptr<Object> obj);
+
+	private:
+		hashtable<long long, Status> status_table_;
 	};
 }
 
