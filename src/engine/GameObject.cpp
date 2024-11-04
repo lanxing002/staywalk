1dfc1c0f7c28d866de5fb3e7fd846b8a2646f483
lanxing002 <scent_wen@outlook.com>
2024-11-04 15:06:02 +0800

    dump and load

--- src/engine/GameObject.cpp
+++ src/engine/GameObject.cpp
@@ -1,1 +1,38 @@
 #include "GameObject.h"
+#include "Utility.h"
+
+namespace staywalk {
+	bool GameObject::operator==(const GameObject& gameobj) {
+		if (!Object::operator==(gameobj)) return false;
+
+		if (location_ != gameobj.location_) return false;
+		if (scale_ != gameobj.scale_) return false;
+		if (rotation_ != gameobj.rotation_) return false;
+		return true;
+	}
+
+	void GameObject::dump(ofstream& ofstrm){
+		Object::dump(ofstrm);
+		auto check_r = Utility::check_ofstream(ofstrm);
+		assert(check_r);
+		ofstrm.write(reinterpret_cast<char*>(&location_), sizeof location_);
+		ofstrm.write(reinterpret_cast<char*>(&scale_), sizeof scale_);
+		ofstrm.write(reinterpret_cast<char*>(&rotation_), sizeof rotation_);
+	}
+
+	//shared_ptr<GameObject> GameObject::load(ifstream& ifs){
+	//	auto result = std::make_shared<GameObject>();
+	//	GameObject::placement_load(result, ifs);
+	//	return result;
+	//}
+
+	void GameObject::placement_load(shared_ptr<GameObject> obj, ifstream& ifs){
+		Object::placement_load(obj, ifs);
+		auto check_r = Utility::check_ifstream(ifs);
+		assert(check_r);
+		ifs.read(reinterpret_cast<char*>(&obj->location_), sizeof obj->location_);
+		ifs.read(reinterpret_cast<char*>(&obj->scale_), sizeof obj->scale_);
+		ifs.read(reinterpret_cast<char*>(&obj->rotation_), sizeof obj->rotation_);
+	}
+}
+
---
+++ src/engine/reflect.h
@@ -0,0 +1,7 @@
+#pragma once
+
+ // reflect property
+#define sw_Prop
+
+// reflect function
+#define sw_Func
