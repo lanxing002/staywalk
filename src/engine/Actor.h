2eafd0d1c318bbecbd70e838cdf80afd5e186953
lanxing002 <scent_wen@outlook.com>
2024-11-06 19:52:13 +0800

    stash

--- src/engine/Actor.h
+++ src/engine/Actor.h
@@ -1,22 +1,21 @@
 #pragma once
 #include "GameObject.h"
 #include "StaticMeshComponent.h"
+#include "Utility.h"
 namespace staywalk {
 	class Actor : public GameObject{
 	public:
-		Actor();
-		Actor(const std::string& name) : name_(name) {}
+		Actor(const string& name = "0-actor");
 
 		void tick(float delta) override;
 
 		ObjectType get_type_value() override { return ObjectType::Actor; }
 		void set_sm_comp(const shared_ptr<StaticMeshComponent> comp) { sm_comp_ = comp; }
 
+
 		bool operator==(const Actor& gameobj);
-
 		void dump(ofstream& ofs, Dumper& dumper) override;
-		static shared_ptr<Actor> load(ifstream& ifs, Loader& loader);
-
+		LOAD_TARGET(Actor);
 	protected:
 		static void placement_load(shared_ptr<Actor> obj, ifstream& ifs, Loader& loader);
 
--- src/engine/GameComponent.h
+++ src/engine/GameComponent.h
@@ -3,6 +3,17 @@
 namespace staywalk{
 	class GameComponent	: public Object{
 	public:
+		GameComponent(const string& name = "0-comp")
+			:Object(name) {}
+
+		bool operator==(const GameComponent& rhs) {
+			return Object::operator==(rhs);
+		}
+
+		//void dump(ofstream& ofs, Dumper& dumper);
+		static shared_ptr<GameComponent> load(ifstream& ifs, Loader& loader);
+	protected:
+		static void placement_load(shared_ptr<GameComponent> obj, ifstream& ifs);
 	};
 }
 
