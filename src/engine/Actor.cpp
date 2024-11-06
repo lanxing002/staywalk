e5e433d473b9d14afce91f3c694e39ec01a95db4
lanxing002 <scent_wen@outlook.com>
2024-11-06 19:52:13 +0800

    stash

--- src/engine/Actor.cpp
+++ src/engine/Actor.cpp
@@ -3,7 +3,8 @@
 #include "Engine.h"
 
 namespace staywalk {
-	Actor::Actor() {
+	Actor::Actor(const string& name)
+		: GameObject(name){
 	}
 
 	void Actor::tick(float delta) {
@@ -13,7 +14,7 @@ namespace staywalk {
 		if (this == &rhs) return true;
 		if (!GameObject::operator==(rhs)) return false;
 		if (name_ != rhs.name_) return false;
-		if (Utility::euqals(sm_comp_, rhs.sm_comp_)) return false;
+		if (!Utility::euqals(sm_comp_, rhs.sm_comp_)) return false;
 		return true;
 	}
 
@@ -21,8 +22,6 @@ namespace staywalk {
 		GameObject::dump(ofs, dumper);
 		auto check_r = Utility::check_ofstream(ofs);
 		assert(check_r);
-
-		Utility::write_to_stream(name_, ofs);
 		if(sm_comp_){
 			Utility::write_to_stream(sm_comp_->get_guid(), ofs);
 			dumper.dump_in_file(sm_comp_);
@@ -32,27 +31,17 @@ namespace staywalk {
 		}
 	}
 
-	shared_ptr<Actor> Actor::load(ifstream& ifs, Loader& loader){
-		auto result = std::make_shared<Actor>();
-		Actor::placement_load(result, ifs, loader);
-		return result;
-	}
-
 	void Actor::placement_load(shared_ptr<Actor> obj, ifstream& ifs, Loader& loader){
 		GameObject::placement_load(obj, ifs);
 		auto check_r = Utility::check_ifstream(ifs);
 		assert(check_r);
-
-		decltype(obj->name_.size()) name_len;
-		ifs.read(reinterpret_cast<char*>(&name_len), sizeof name_len);
-		obj->name_.resize(name_len);
-		ifs.read((obj->name_.data()), name_len);
-
-		idtype sm_comp_id = kInvalidId;
-		ifs.read(reinterpret_cast<char*>(&sm_comp_id), sizeof sm_comp_id);
+		idtype sm_comp_id = Utility::load_from_stream<idtype>(ifs);
 		if (sm_comp_id != kInvalidId) {
 			ObjectType ot;
-			obj->sm_comp_ = std::dynamic_pointer_cast<StaticMeshComponent>(loader.load_in_file(sm_comp_id, ot));
+			auto cc = loader.load_in_file(sm_comp_id, ot);
+			auto dd = std::dynamic_pointer_cast<StaticMeshComponent>(cc);
+			auto ccc = dynamic_cast<StaticMeshComponent*>(cc.get());
+			obj->sm_comp_ = dd;
 			assert(ot == ObjectType::StaticMeshComponent);
 		}
 
