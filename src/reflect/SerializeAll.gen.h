b43bea402da10a24a2325010fd673ea61aaf78da
lanxing002 <lanxing002@gmail.com>
2024-11-12 22:47:00 +0800

    stash

--- src/reflect/SerializeAll.gen.h
+++ src/reflect/SerializeAll.gen.h
@@ -1,6 +1,6 @@
-#include "Common.gen.h"
-
-void Serializer<staywalk::GameObject>::dump(const staywalk::GameObject& obj, ofstream& ofs, Dumper& dumper) {
+namespace staywalk{
+template<>
+void staywalk::reflect::Serializer<GameObject>::dump(const GameObject& obj, ofstream& ofs, Dumper& dumper) {
     Serializer<Object>::dump(obj, ofs, dumper);
     dumper.write(obj.location, ofs);
     dumper.write(obj.scale, ofs);
@@ -8,12 +8,33 @@ void Serializer<staywalk::GameObject>::dump(const staywalk::GameObject& obj, ofs
 }
 
 
-void Serializer<staywalk::GameComponent>::dump(const staywalk::GameComponent& obj, ofstream& ofs, Dumper& dumper) {
+template<>
+void staywalk::reflect::Serializer<GameObject>::load(GameObject& obj, ifstream& ifs, Loader& loader) {
+    Serializer<Object>::load(obj, ifs, loader);
+    loader.read(obj.location, ifs);
+    loader.read(obj.scale, ifs);
+    loader.read(obj.rotation, ifs);
+}
+}
+
+
+namespace staywalk{
+template<>
+void staywalk::reflect::Serializer<GameComponent>::dump(const GameComponent& obj, ofstream& ofs, Dumper& dumper) {
     Serializer<Object>::dump(obj, ofs, dumper);
 }
 
 
-void Serializer<staywalk::RObject>::dump(const staywalk::RObject& obj, ofstream& ofs, Dumper& dumper) {
+template<>
+void staywalk::reflect::Serializer<GameComponent>::load(GameComponent& obj, ifstream& ifs, Loader& loader) {
+    Serializer<Object>::load(obj, ifs, loader);
+}
+}
+
+
+namespace staywalk{
+template<>
+void staywalk::reflect::Serializer<RObject>::dump(const RObject& obj, ofstream& ofs, Dumper& dumper) {
     Serializer<Object>::dump(obj, ofs, dumper);
     dumper.write(obj.glid, ofs);
     dumper.write(obj.valid, ofs);
@@ -21,12 +42,33 @@ void Serializer<staywalk::RObject>::dump(const staywalk::RObject& obj, ofstream&
 }
 
 
-void Serializer<staywalk::RTex>::dump(const staywalk::RTex& obj, ofstream& ofs, Dumper& dumper) {
+template<>
+void staywalk::reflect::Serializer<RObject>::load(RObject& obj, ifstream& ifs, Loader& loader) {
+    Serializer<Object>::load(obj, ifs, loader);
+    loader.read(obj.glid, ifs);
+    loader.read(obj.valid, ifs);
+    loader.read(obj.path, ifs);
+}
+}
+
+
+namespace staywalk{
+template<>
+void staywalk::reflect::Serializer<RTex>::dump(const RTex& obj, ofstream& ofs, Dumper& dumper) {
     Serializer<RObject>::dump(obj, ofs, dumper);
 }
 
 
-void Serializer<staywalk::RMesh>::dump(const staywalk::RMesh& obj, ofstream& ofs, Dumper& dumper) {
+template<>
+void staywalk::reflect::Serializer<RTex>::load(RTex& obj, ifstream& ifs, Loader& loader) {
+    Serializer<RObject>::load(obj, ifs, loader);
+}
+}
+
+
+namespace staywalk{
+template<>
+void staywalk::reflect::Serializer<RMesh>::dump(const RMesh& obj, ofstream& ofs, Dumper& dumper) {
     Serializer<RObject>::dump(obj, ofs, dumper);
     dumper.write(obj.vertices_, ofs);
     dumper.write(obj.indices_, ofs);
@@ -34,28 +76,81 @@ void Serializer<staywalk::RMesh>::dump(const staywalk::RMesh& obj, ofstream& ofs
 }
 
 
-void Serializer<staywalk::Resource>::dump(const staywalk::Resource& obj, ofstream& ofs, Dumper& dumper) {
+template<>
+void staywalk::reflect::Serializer<RMesh>::load(RMesh& obj, ifstream& ifs, Loader& loader) {
+    Serializer<RObject>::load(obj, ifs, loader);
+    loader.read(obj.vertices_, ifs);
+    loader.read(obj.indices_, ifs);
+    loader.read(obj.mat_, ifs);
+}
+}
+
+
+namespace staywalk{
+template<>
+void staywalk::reflect::Serializer<Resource>::dump(const Resource& obj, ofstream& ofs, Dumper& dumper) {
     Serializer<Object>::dump(obj, ofs, dumper);
     dumper.write(obj.source, ofs);
     dumper.write(obj.dump_dir, ofs);
 }
 
 
-void Serializer<staywalk::Shader>::dump(const staywalk::Shader& obj, ofstream& ofs, Dumper& dumper) {
+template<>
+void staywalk::reflect::Serializer<Resource>::load(Resource& obj, ifstream& ifs, Loader& loader) {
+    Serializer<Object>::load(obj, ifs, loader);
+    loader.read(obj.source, ifs);
+    loader.read(obj.dump_dir, ifs);
+}
+}
+
+
+namespace staywalk{
+template<>
+void staywalk::reflect::Serializer<Shader>::dump(const Shader& obj, ofstream& ofs, Dumper& dumper) {
     Serializer<Resource>::dump(obj, ofs, dumper);
     dumper.write(obj.code, ofs);
     dumper.write(obj.shader_type, ofs);
 }
 
 
-void Serializer<staywalk::StaticMeshComponent>::dump(const staywalk::StaticMeshComponent& obj, ofstream& ofs, Dumper& dumper) {
+template<>
+void staywalk::reflect::Serializer<Shader>::load(Shader& obj, ifstream& ifs, Loader& loader) {
+    Serializer<Resource>::load(obj, ifs, loader);
+    loader.read(obj.code, ifs);
+    loader.read(obj.shader_type, ifs);
+}
+}
+
+
+namespace staywalk{
+template<>
+void staywalk::reflect::Serializer<StaticMeshComponent>::dump(const StaticMeshComponent& obj, ofstream& ofs, Dumper& dumper) {
     Serializer<GameComponent>::dump(obj, ofs, dumper);
     dumper.write(obj.meshs, ofs);
 }
 
 
-void Serializer<staywalk::Actor>::dump(const staywalk::Actor& obj, ofstream& ofs, Dumper& dumper) {
+template<>
+void staywalk::reflect::Serializer<StaticMeshComponent>::load(StaticMeshComponent& obj, ifstream& ifs, Loader& loader) {
+    Serializer<GameComponent>::load(obj, ifs, loader);
+    loader.read(obj.meshs, ifs);
+}
+}
+
+
+namespace staywalk{
+template<>
+void staywalk::reflect::Serializer<Actor>::dump(const Actor& obj, ofstream& ofs, Dumper& dumper) {
     Serializer<GameObject>::dump(obj, ofs, dumper);
     dumper.write(obj.sm_comp, ofs);
 }
+
+
+template<>
+void staywalk::reflect::Serializer<Actor>::load(Actor& obj, ifstream& ifs, Loader& loader) {
+    Serializer<GameObject>::load(obj, ifs, loader);
+    loader.read(obj.sm_comp, ifs);
+}
+}
+
 
