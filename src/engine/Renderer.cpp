6f4271ebb0a5c27e51af56c0d99d520f025fe448
lanxing002 <scent_wen@outlook.com>
2024-11-17 12:39:20 +0800

    ui build

--- src/engine/Renderer.cpp
+++ src/engine/Renderer.cpp
@@ -1,1 +1,8 @@
 #include "Renderer.h"
+#include "rhi.h"
+
+void Renderer::render(double delta, unsigned long long count)
+{
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the 
+}
