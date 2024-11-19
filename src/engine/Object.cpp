be395cccad71fb70422af44ee3d780d412c4951d
lanxing002 <scent_wen@outlook.com>
2024-11-19 10:45:20 +0800

    enum ui bind

--- src/engine/Object.cpp
+++ src/engine/Object.cpp
@@ -11,4 +11,20 @@ namespace staywalk{
 	Object::Object(idtype load_id, const string& name)
 		: guid_(load_id), name(name){
 	}
+	
+	void Object::construct_ui(bool open, const std::string& header_name) {
+		auto flags = open ? ImGuiTreeNodeFlags_DefaultOpen : ImGuiTreeNodeFlags_None;
+        if (ImGui::CollapsingHeader(header_name.c_str(), flags)) {
+            //ImGui::Separator();
+
+            if (ImGui::BeginTable("##properties", 2, ImGuiTableFlags_Resizable))
+            {
+                ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthFixed);
+                ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthStretch, 1.0f);
+				construct_basic_ui();
+                ImGui::EndTable();
+            }
+        }
+		construct_obj_ui();
+	}
 }
