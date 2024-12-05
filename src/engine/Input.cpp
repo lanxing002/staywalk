40fa32b96b44324b55e768ead6f8d68b0f432aa9
lanxing002 <lanxing002@lanxing.com>
2024-12-05 19:01:26 +0800

    camera controll

--- src/engine/Input.cpp
+++ src/engine/Input.cpp
@@ -1,17 +1,17 @@
 #include "Input.h"
 
 void staywalk::MouseDevice::clear(){
-	//for (int i = 0; i < (int)Keyboard::COUNT; i++) {
-	//	state_[i][0] = false;
-	//	state_[i][1] = false;
-	//}
+	for (int i = 0; i < 3; i++) {
+		state_[i][0] = false;
+		state_[i][1] = false;
+	}
 
 	mouse_offset = vec2(.0, .0);
 	wheel_offset = vec2(.0, .0);
 }
 
 void staywalk::KeyBoardDevice::clear(){
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < (int)Keyboard::COUNT; i++) {
 		state_[i][0] = false;
 		state_[i][1] = false;
 	}
