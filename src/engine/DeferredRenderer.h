5625e3d21c1a24683da7bfe948057d193079675a
lanxing002 <scent_wen@outlook.com>
2025-01-15 17:39:13 +0800

    refactor render pass

--- src/engine/DeferredRenderer.h
+++ src/engine/DeferredRenderer.h
@@ -14,11 +14,35 @@ namespace staywalk {
 
 		void destroy() override;
 
-		void render_main(RenderInfo& renderinfo);
+		void render_main();
+
+		void render_post0();
+		
+		void render_post1();
+
+		void render_screen();
+
+		void render_custom_rt(RenderTargetRef rt);
 
 		void render(double delta, unsigned long long count) override;
 
+		// render all meshes, for render target or main pass
+		void render_all_mesh(RenderInfo& render_info);
+
 	private:
-		Ref<GBuffer> gbuffer_;
+		// render pipeline start
+
+			// mainpass
+			Ref<GBuffer> mainpass_gbuffer_;
+
+
+			// post pass
+
+
+			// render to screen pass
+
+
+
+		// render pipeline end
 	};
 }
